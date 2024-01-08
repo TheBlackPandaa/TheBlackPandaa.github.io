f8565c53b9efcdbe8cef2af24fd0203922951d86
Andrzej Maliszewski <adnrzej@netmb.pl>
2024-01-08 19:54:12 +0100

    at it again

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'http://localhost:8080/v1/tasks';
+  var apiRoot = 'https://d0e8dc1c-33f3-4a86-a775-b61f461c9a85-00-26quprb85qgd2.spock.replit.dev/v1/tasks';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
